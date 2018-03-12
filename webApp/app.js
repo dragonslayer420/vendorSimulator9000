6dc0ba87d8eb485f9cf62f60ce22f907fcfc6aa9
hiimmario <m.reichart@hotmail.com>
2018-03-12 15:20:40 +0100

    remove web3 express

--- webApp/app.js
+++ webApp/app.js
@@ -1,12 +1,6 @@
 var express = require("express");
 var app = express();
 
-var Web3 = require("web3");
-var myweb3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/Prmmb8q9E0i0tPw8TDfV"));
-
-console.log("Connected: ", myweb3.isConnected(), myweb3.currentProvider);
-console.log("syncing: ", myweb3.eth.syncing, ", Latest Block: ", myweb3.eth.blockNumber);
-
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
